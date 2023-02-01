837ff4bb38d846eef8e86923493e0c1795a29d84
majuojuma <majuo8871@gmail.com>
2023-02-01 14:00:42 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { RegistorComponent } from './registor/registor.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path:'',
+  component:LoginComponent
+},{
+  path:'registor',
+  component:RegistorComponent,
+},
+{
+  path:'contact',
+  component:ContactComponent,
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
